cb2559d4805b53f5ddea18bfa2d5a01cc14b3fd6
plesett <1540907804@qq.com>
2020-11-10 10:16:06 +0800

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,7 @@ import { BaseExceptionFilter } from "@nestjs/core"
 
 // mongodb
 export const config = {
-    SECRET: 'asQW1eedASD!12!@3asd1!#Drg%H5jh%J^H31F@#f!@#F!#F',
+    SECRET: '******************',
     DB: 'mongodb://127.0.0.1:27017/test'
 }
 
@@ -10,19 +10,19 @@ export const config = {
 export const options={
     port: 6379,
     host: '127.0.0.1',
-    password: 'zkpkzkpk'
+    password: '******************',
 }
 
 // 阿里云oos
 export const AliOOS = {
     region: 'oss-cn-hangzhou',
-    accessKeyId: '********',
-    accessKeySecret: '*******',
-    bucket: '*****'
+    accessKeyId: '******************',
+    accessKeySecret: '******************',
+    bucket: '******************',
 }
 
 // 腾讯位置服务 // https://lbs.qq.com/service/webService/webServiceGuide/webServiceIp
 export const TenXunMap ={
-    key: 'A4WBZ-AW332-EDHUE-CHEIK-63K6O-RLFTL',
+    key: '******************',
     url: 'https://apis.map.qq.com/ws/location/v1/ip' // <----次接口无需改动
 }
